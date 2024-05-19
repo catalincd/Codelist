ff9e5b207deda0414ef74a4d5f64741d16f9cec6
catalincd <cc.dumitrescu02@gmail.com>
2024-05-19 21:56:43 +0300

    Package cleanup

--- frontend/src/components/CodeRenderer.jsx
+++ frontend/src/components/CodeRenderer.jsx
@@ -1,12 +1,10 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-import rehypeMermaid from 'rehype-mermaid'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark'
 import remarkRehype from 'remark-rehype'
 import { unified } from 'unified'
-import Mermaid from "react-mermaid2"
 
 const CodeRenderer = (props) => {
     const { children, className, node, ...rest } = props
@@ -23,11 +21,6 @@ const CodeRenderer = (props) => {
     )
 }
 
-
-const MermaidRenderer = (props) => {
-
-    return <Mermaid chart={props.children} />
-}
 
 
 export default CodeRenderer
